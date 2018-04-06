e97b09967d04c8217dec80eeebb39d945b08b74c
DESKTOP-MEIG9TE\hoangthai <hoangthai0302@gmail.com>
2018-04-07 00:48:50 +0700

    update

--- webpack.config.js
+++ webpack.config.js
@@ -6,50 +6,41 @@ const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const OptimizeCssAssetsWebpackPlugin = require('optimize-css-assets-webpack-plugin');
 const autoprefixer = require('autoprefixer');
 
+console.log("Current mode: " + process.env.NODE_ENV)
+
 const config = {
-	entry: ['babel-polyfill', './src/app.module.js', './src/vendor.module.js'
-	],
+	entry: ['babel-polyfill', './src/app.module.js', './src/vendor.module.js'],
 	devtool: 'source-map',
 	output: {
 		filename: 'libs/[name].bundle.js',
-        path: path.resolve(__dirname, 'dist')
+		path: path.resolve(__dirname, 'dist')
 	},
 	module: {
-		rules: [
-			{
+		rules: [{
 				test: /\.js$/,
 				exclude: /node_modules/,
 				loader: ['ng-annotate-loader', 'babel-loader']
 			},
 			{
 				test: /\.(s?)css$/,
-				use: ['css-loader', 
-				{
-					                loader: 'postcss-loader',
-					                options: {
-					                    plugins: (loader) => [autoprefixer()]
-					                }
-					            
-				},
-				'sass-loader']
-				// .concat(ExtractTextPlugin.extract({
-				// 	fallback: 'style-loader',
-                //     use: ['css-loader',
-                //             {
-                //                 loader: 'postcss-loader',
-                //                 options: {
-                //                     plugins: (loader) => [autoprefixer()]
-                //                 }
-                //             },
-                //         'sass-loader'
-                //     ]
-				//   })),
+				use:['css-hot-loader'].concat(ExtractTextPlugin.extract({
+					fallback: 'style-loader',
+					use: ['css-loader',
+						{
+							loader: 'postcss-loader',
+							options: {
+								plugins: (loader) => [autoprefixer()]
+							}
+						},
+						'sass-loader'
+					]
+				})) 
 			},
 			{
 				test: /\.(png|jpg|jpeg|gif|svg)$/,
-                use: ['file-loader?name=[name].[ext]',
-                    'image-webpack-loader'
-                ]
+				use: ['file-loader?name=[name].[ext]',
+					'image-webpack-loader'
+				]
 			},
 			{
 				test: /\.(eot|ttf|woff|woff2)$/,
@@ -58,39 +49,40 @@ const config = {
 					name: './fonts/[name].[ext]'
 				}
 			},
-			{ test: /\.html$/, loader: 'html-loader' }
+			{
+				test: /\.html$/,
+				loader: 'html-loader'
+			}
 		]
 	},
 	plugins: [
-		// new webpack.optimize.UglifyJsPlugin({
-		// 	comments: false,
-		// 	sourceMap: true,
-		// }), // for mifiying js
+		new webpack.optimize.UglifyJsPlugin({
+			comments: false,
+			sourceMap: true,
+		}), // for mifiying js
 		new webpack.optimize.CommonsChunkPlugin({
 			name: 'vendor',
 			filename: 'libs/[name].bundle.js'
 		}),
 		new HtmlWebpackPlugin({
-            template: './src/index.html',
-            inject:'body',
-            hash:true
+			template: './src/index.html',
+			inject: 'body',
+			hash: true
 		}),
 		new webpack.ProvidePlugin({
 			jQuery: 'jquery',
 			$: 'jquery',
 			jquery: 'jquery',
-			Pikaday:'Pikaday'
 		}),
-        new ExtractTextPlugin('styles/styles.css'),
-        new webpack.HotModuleReplacementPlugin()
-		//new OptimizeCssAssetsWebpackPlugin()
+		new ExtractTextPlugin('styles/styles.css'),
+		new webpack.HotModuleReplacementPlugin()
 	],
 	devServer: {
 		port: 3000,
 		contentBase: './dist',
-        historyApiFallback: true,
-        compress:true,
-        hot:false
+		historyApiFallback: true,
+		compress: true,
+		hot: true
 	}
 };
 
